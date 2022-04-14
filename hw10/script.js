262628f01dc3e88e872d8451829aa430276e9d9e
Braun <braun.ufo@gmail.com>
2022-04-14 10:49:36 +0300

    hw10 solved

--- hw10/script.js
+++ hw10/script.js
@@ -1,8 +1,27 @@
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+//
+// if(numOrStr === null || numOrStr.trim() === '' || isNaN( +numOrStr )) {
+//     console.log('вы отменили')
+// } else {
+//     console.log('OK!')
+// }
+
+
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-if(numOrStr === null || numOrStr.trim() === '' || isNaN( +numOrStr )) {
-    console.log('вы отменили')
-} else {
-    console.log('OK!')
+switch (true) {
+    case numOrStr === null:
+        console.log('вы отменили')
+        break
+    case numOrStr.trim() === "":
+        console.log('Empty String');
+        break
+    case (isNaN(+numOrStr)):
+        console.log('number is Ba_NaN')
+        break
+    default: {
+        console.log('OK!')
+    }
 }
